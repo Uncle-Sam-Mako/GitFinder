891e0ffe315890032ac43d2d3b3c79dce8146e16
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-06-14 21:56:50 +0200

    Update index.html, setup tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    colors: {
+      'white' : "#fff",
+      'boldColor': '#0f172a',
+      'regularColor': '#64748b',
+      'borderColor': "#94a3b8" 
+    },
     extend: {},
   },
   plugins: [],
