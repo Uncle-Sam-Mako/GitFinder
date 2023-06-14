a80e08f80aa81552f0c5234ca4e42b9f8c545cce
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-06-14 23:24:25 +0200

    Update index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
       'white' : "#fff",
       'boldColor': '#0f172a',
       'regularColor': '#64748b',
-      'borderColor': "#94a3b8" 
+      'borderColor': "#94a3b8",
+      'skeleton' : "#cbd5e1"
     },
     extend: {},
   },
