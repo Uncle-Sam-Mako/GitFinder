11a36f237a3ba0c130664e4fc9f32c74a136dc78
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-06-15 14:11:34 +0200

    Update index.html : add .user-modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    colors: {
-      'white' : "#fff",
-      'boldColor': '#0f172a',
-      'regularColor': '#64748b',
-      'borderColor': "#e2e8f0",
-      'skeleton' : "#94a3b8"
+    extend: {
+      colors: {
+        'white' : "#fff",
+        'boldColor': '#0f172a',
+        'regularColor': '#64748b',
+        'borderColor': "#e2e8f0",
+        'skeleton' : "#cbd5e1"
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
