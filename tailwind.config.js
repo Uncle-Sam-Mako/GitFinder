8281520ed2caec3c95a85dadf58ea64fc1b44dae
Uncle-Sam-Mako <bwsamuel6@gmail.com>
2023-06-21 23:08:21 +0200

    Edit tailwindcss theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'white' : "#fff",
-        'boldColor': '#0f172a',
-        'regularColor': '#64748b',
-        'borderColor': "#e2e8f0",
         'skeleton' : "#cbd5e1"
       },
     },
